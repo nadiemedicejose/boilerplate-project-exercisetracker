f6415c09dc393f2fe89c8f6fe4d5ef658c2b60eb
Jose Estrada <j.estrada49@icloud.com>
2021-07-01 07:10:02 -0700

    database setup

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
+// Database setup
+const mongoose = require('mongoose')
+const db_uri = process.env.DB_URI
+mongoose.connect(db_uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
